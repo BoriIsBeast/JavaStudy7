5bb4da5485fe144ddbe52f877dd1b289423e2a7a
BoriIsBeast <wlsdn147@naver.com>
2022-01-13 16:33:38 +0900

    make DTO

---
+++ src/com/iu/s1/human/HumanDTO.java
@@ -0,0 +1,14 @@
+package com.iu.s1.human;
+
+public class HumanDTO {
+
+private String name;	
+private String nick;	
+private String email;	
+private String phone;
+private String birth;
+
+
+	
+
+}
---
+++ src/com/iu/s1/human/JoosoUtil.java
@@ -0,0 +1,8 @@
+package com.iu.s1.human;
+
+public class JoosoUtil {
+
+	public JoosoUtil(){
+		
+	}
+}
